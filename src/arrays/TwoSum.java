1145e1d843e99ad3c22d2af98fd4b52a4b199c90
padhav <adhavpratap@gmail.com>
2026-02-01 17:08:09 +0530

    Two Sum

--- src/arrays/TwoSum.java
+++ src/arrays/TwoSum.java
@@ -1,4 +1,33 @@
 package arrays;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class TwoSum {
+
+    static void main() {
+        int [] arr={1,2,3,4,5,6,7,8};
+        int target=12;
+        int[] ints = bruteForce(arr, target);
+        System.out.println(Arrays.toString(ints));
+
+        int [] optimised=optimised(arr,target);
+    }
+
+    private static int[] optimised(int[] arr, int target) {
+
+//        Map<>
+
+        return null;
+    }
+
+    private static int[] bruteForce(int[] arr, int target) {
+
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr.length; j++){
+                if(arr[i]+arr[j]==target) return new int[]{i,j};
+            }
+        }
+        return new int[0];
+    }
 }
