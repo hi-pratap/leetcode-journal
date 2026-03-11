a5bcd29447a23d641fc6dff724a84167e68f05dd
padhav <adhavpratap@gmail.com>
2026-03-11 17:48:13 +0530

    Comapre two numbers

---
+++ src/math/CompareTwoNumbers.java
@@ -0,0 +1,11 @@
+package math;
+
+public class CompareTwoNumbers {
+    static void main() {
+        int a=2;
+        int ba=3;
+
+        int max = a > ba ? a : ba;
+        System.out.println(max);
+    }
+}
