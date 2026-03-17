620426f905fc0b459d92dd45a73f8e65bdb32ce4
padhav <adhavpratap@gmail.com>
2026-03-17 19:03:26 +0530

    Second Largest element

---
+++ src/arrays/SecondLargestElement.java
@@ -0,0 +1,27 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class SecondLargestElement {
+    static void main() {
+        int [] arr={1,2,3,4,5};
+        System.out.println(secondLargestElementInArr(arr));
+    }
+
+    private static int secondLargestElementInArr(int[] arr) {
+        int max = 0;
+        int secondMax = 0;
+        int n = arr.length-1; //4
+        while (n >= 0) {
+            if (arr[n]>max) {//2
+                secondMax=max;
+                max=arr[n];//2
+            }else if(arr[n]>secondMax  && arr[n]<max){//false
+                secondMax=max;//0
+            }
+            n--;
+        }
+        return secondMax;
+    }
+
+}
