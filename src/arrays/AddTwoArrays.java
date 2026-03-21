8ca1dc0a0c9404cb5ec2a08d32e665a13e0243eb
padhav <adhavpratap@gmail.com>
2026-03-21 19:33:33 +0530

    Add two arrays

--- src/arrays/AddTwoArrays.java
+++ src/arrays/AddTwoArrays.java
@@ -6,24 +6,23 @@ public class AddTwoArrays {
         int[] arr2={3,4,9};
     addTwoArrays(arr1,arr2);
     }
-// 1 2 3 4 9
-//     3 4 9
-// 1 2 6 9 0
+
     private static void addTwoArrays(int[] arr1, int[] arr2) {
-int n=arr1.length;
-        int [] ans=new int[arr1.length];
-        int i=arr1.length;
-        int j=arr2.length;
+        int [] ans=new int[Math.max(arr1.length,arr2.length)+1];
+        int i=arr1.length-1;
+        int j=arr2.length-1;
+        int k=ans.length-1;
         int carry=0;
-        int sum=0;
-        while(i>0||j>0||carry!=0){
-            sum=carry;
-            sum=arr1[i]+arr2[j];
-            carry=sum%10;
 
-            ans[i]
+        while(k>=0){
+            int sum=0;
+            if(i>=0) sum+=arr1[i];
+            if(j>=0) sum+=arr2[j];
 
-            n--;
+            sum+=carry;
+            ans[k]=sum%10;
+            carry=sum/10;
+            i--;j--;k--;
         }
 
     }
