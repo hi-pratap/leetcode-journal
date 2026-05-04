5d86af8b0391b881829e2f19c51897402d1677b9
padhav <adhavpratap@gmail.com>
2026-05-04 17:12:41 +0530

    +1

--- src/arrays/twopointer/MaxConsecativeOnes.java
+++ src/arrays/twopointer/MaxConsecativeOnes.java
@@ -1,4 +1,30 @@
 package arrays.twopointer;
 
 public class MaxConsecativeOnes {
+    static void main() {
+        int[] arr={1,1,0,0,1,0,1,1,1,1};
+        maxConsecativeOnes(arr);
+    }
+
+    private static void maxConsecativeOnes(int[] arr) {
+
+        int i=0;
+        int j=0;
+        int n = arr.length - 1;
+        int max=Integer.MIN_VALUE;
+        int count=0;
+        while(n>=0){
+                if(arr[j]==1){
+                    j++;
+                }else{
+                    count=j-i;
+                    if(count>max) max=j-i;
+                    j++;
+                            i=j;
+                }
+            if(j-i>max) max=j-i;
+            n--;
+        }
+        System.out.println(max);
+    }
 }
