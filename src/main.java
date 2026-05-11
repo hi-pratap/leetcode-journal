c7c494edefc6f0a1277b275aa30218f2b1d1a1dd
padhav <adhavpratap@gmail.com>
2026-05-11 18:53:01 +0530

    +1

--- src/main.java
+++ src/main.java
@@ -2,12 +2,13 @@
 public class main{
 
     static void main() {
-// starting the journey 
-//dummy lush
-// dummy push 2
-// dummy push 3
-// dummy push 5
-// dummy push 6
+        // starting the journey
+        //dummy lush
+        // dummy push 2
+        // dummy push 3
+        // dummy push 5
+        // dummy push 6
+        //Time-complexity lecture
         System.out.println("Hello");
     }
 }
